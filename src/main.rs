ff0e595723f1bda1d891676356ab5092620203a7
Matthew Patchava <mattpatchava@hotmail.com>
2025-06-08 18:10:09 +1000

    feat: scaffold CLI with add, rm, show, and list subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about = "A CLI password storage utility", long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {
+        #[arg(short, long)]
+        encrypted: bool,
+        #[arg(short, long)]
+        username: String,
+        #[arg(short, long)]
+        password: String,
+    },
+    Rm {
+        #[arg(short, long)]
+        username: String,
+    },
+    Show {
+        #[arg(short, long)]
+        username: String,
+    },
+    List,
+}
+
+fn add(encrypted: bool, username: String, password: String) {
+    if encrypted {
+        println!("Adding encrypted entry:\n{}: {}", username, password);
+    } else {
+        println!("Adding plaintext entry:\n{}: {}", username, password);
+    }
+}
+
+fn rm(username: String) {
+    println!("Removing entry: {}", username);
+}
+
+fn show(username: String) {
+    println!("Showing password for: {}", username);
+}
+
+fn list() {
+    println!("Listing all passwords");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse();
+
+    match args.command {
+        Commands::Add {
+            encrypted,
+            username,
+            password,
+        } => {
+            if encrypted {
+                add(true, username, password)
+            } else {
+                add(false, username, password)
+            }
+        }
+        Commands::Rm { username } => rm(username),
+        Commands::Show { username } => show(username),
+        Commands::List => list(),
+    }
 }
