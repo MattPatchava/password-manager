0c95a3e0faebf970d2b8e22d9609fb64ea2fc7cc
Matt Patchava <mattpatchava@outlook.com>
2025-06-29 11:03:56 +1000

    refactor: modularise project into io, hashing, models, and store modules

---
+++ src/hashing/password.rs
@@ -0,0 +1,25 @@
+use anyhow::{anyhow, Result};
+use base64::{engine::general_purpose, Engine};
+use rand::rngs::OsRng;
+use rand::RngCore;
+
+pub fn generate_salt() -> String {
+    let mut salt = [0u8; 32];
+    let mut rng: OsRng = OsRng::default();
+
+    rng.fill_bytes(&mut salt);
+
+    general_purpose::STANDARD.encode(salt)
+}
+
+pub fn hash_password(master_password: &str, salt: Vec<u8>) -> Result<[u8; 32]> {
+    let mut aes_encryption_key: [u8; 32] = [0u8; 32];
+
+    let argon2 = argon2::Argon2::default();
+
+    argon2
+        .hash_password_into(&master_password.as_bytes(), &salt, &mut aes_encryption_key)
+        .map_err(|e| anyhow!(e))?;
+
+    Ok(aes_encryption_key)
+}
